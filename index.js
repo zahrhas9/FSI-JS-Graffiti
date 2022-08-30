4f9e877258a1c2668a77938ed653bec7f411281c
Zahra Hassan <zahrhas9@gmail.com>
2022-08-30 18:40:37 -0500

    flipped images aligned text to left

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Select some elements...
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+header.style.fontFamily = "arial"
+header.style.color = "black"
+let dogImages = document.querySelectorAll('.dog-image')
+for (let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+    dogImages[i].style.transform = 'scaleY(-1)'
+
+}
+let dogNames = document.querySelectorAll('.dog-name')
+for (let i = 0; i < dogNames.length; i++){
+    dogNames[i].style.textAlign = 'left'
+
+}
+let footer = document.querySelector('.footer')
+footer.style.color = "pink";
